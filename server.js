3da69e2479f7f4a857f4ed2a6834372539525a04
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-14 12:40:53 -0700

    Finish 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const data = require('./mockData');
+var path = require("path");
 
 const urlLogger = (request, response, next) => {
   console.log('Request URL:', request.url);
@@ -11,12 +12,20 @@ const timeLogger = (request, response, next) => {
   next();
 };
 app.use(urlLogger, timeLogger);
+
 app.use(express.static('public'));
+
 app.get('/', (request, response) => {
 });
+
 app.get('/json', (request, response) => {
   response.status(200).json(data);
 });
+
+app.get('*', function (req, res) {
+    res.status(404).sendFile(path.join(__dirname + '/public/errorPage.html'));
+  });
+
 app.listen(3000, () => {
   console.log('Express Intro running on localhost:3000');
 });
